4a1a907fdb3fe29cbd4b0feccf2eb73957545097
Joao Inocencio <inocenciojoaovitor100@gmail.com>
2024-05-14 23:26:41 -0300

    respostas para o usuario

--- api/src/modelos/Usuario.go
+++ api/src/modelos/Usuario.go
@@ -1,8 +1,12 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
-//Usuario representa um usuário da rede social
+// Usuario representa um usuário da rede social
 type Usuario struct {
 	ID       uint64    `json:"id,omitempty"`
 	Nome     string    `json:"nome,omitempty"`
@@ -10,4 +14,36 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O Nome é obrigatório e não pode estar em branco")
+	}
+	if usuario.Nick == "" {
+		return errors.New("O Nick é obrigatório e não pode estar em branco")
+	}
+	if usuario.Email == "" {
+		return errors.New("O Email é obrigatório e não pode estar em branco")
+	}
+	if usuario.Senha == "" {
+		return errors.New("A senha é obrigatória e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
