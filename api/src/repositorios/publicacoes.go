fcd656a92112341a6fd749b454db70b9da368de1
Joao Inocencio <inocenciojoaovitor100@gmail.com>
2024-06-18 22:19:00 -0300

    buscar por ID e Update publicacoes

--- api/src/repositorios/publicacoes.go
+++ api/src/repositorios/publicacoes.go
@@ -34,3 +34,88 @@ func (repositorio Publicacoes) Criar(publicacao modelos.Publicacao) (uint64, err
 
 	return uint64(ultimoIDInserio), nil
 }
+
+func (repositorio Publicacoes) BuscarPorID(publicacaoID uint64) (modelos.Publicacao, error) {
+	linha, erro := repositorio.db.Query(`
+		select p.*, u.nick from
+		publicacoes p 
+		inner join usuarios u on u.id = p.autor_id where p.id = ?
+	`, publicacaoID)
+
+	if erro != nil {
+		return modelos.Publicacao{}, erro
+	}
+
+	defer linha.Close()
+
+	var publicacao modelos.Publicacao
+
+	if linha.Next() {
+		if erro = linha.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorId,
+			&publicacao.Curtidas,
+			&publicacao.CriadoEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return modelos.Publicacao{}, erro
+		}
+	}
+
+	return publicacao, nil
+}
+
+func (repositorio Publicacoes) Buscar(usuarioID uint64) ([]modelos.Publicacao, error) {
+	linhas, erro := repositorio.db.Query(`
+	select distinct p.*, u.nick from publicacoes p
+	inner join usuarios u on u.id = p.autor_id
+	inner join seguidores s on p.autor_id = s.usuario_id
+	where u.id = ? or s.seguidor_id = ?
+	order by 1 desc`,
+		usuarioID, usuarioID,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+
+	defer linhas.Close()
+
+	var publicacoes []modelos.Publicacao
+
+	for linhas.Next() {
+		var publicacao modelos.Publicacao
+
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorId,
+			&publicacao.Curtidas,
+			&publicacao.CriadoEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return nil, erro
+		}
+
+		publicacoes = append(publicacoes, publicacao)
+	}
+
+	return publicacoes, nil
+}
+
+func (repositorio Publicacoes) Atualizar(publicacaoID uint64, publicacao modelos.Publicacao) error {
+	statement, erro := repositorio.db.Prepare("update publicacoes set titulo = ?, conteudo = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
