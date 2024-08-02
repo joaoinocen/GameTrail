246e81f0c63dcc34e693b9bbd03e37fa24af3432
Joao Inocencio <inocenciojoaovitor100@gmail.com>
2024-08-01 23:20:43 -0300

    Apresentando publicacoes na Home

--- api/src/repositorios/publicacoes.go
+++ api/src/repositorios/publicacoes.go
@@ -73,7 +73,7 @@ func (repositorio Publicacoes) Buscar(usuarioID uint64) ([]modelos.Publicacao, e
 	inner join usuarios u on u.id = p.autor_id
 	inner join seguidores s on p.autor_id = s.usuario_id
 	where u.id = ? or s.seguidor_id = ?
-	order by 1 desc`,
+	`,
 		usuarioID, usuarioID,
 	)
 	if erro != nil {
